bc8b998f11288daf2603e0def01f067b4682e798
Mislav Švaljek <mislavva@gmail.com>
2017-09-06 23:16:37 +0200

    Refactoring code v3... (changed style, app.js updated but not finished yet)

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ function displayTime(where)
     if(where === 'history')
     {
         return function() {
-            //something
+            var hour = this.getHours().toString(),
+                minutes = this.getMinutes().toString();
+           return hour + ':' + (minutes.length === 1 ? '0' + minutes : minutes);
         }.call(currentTime);
     }
     else if(where === 'calculator')
@@ -41,28 +43,84 @@ var calculationData =
     {
         this.numbers.push({
             value: number,
-            operation: operator || null
+            operator: operator || null
         });
     },
     clearNumbers: function()
     {
         this.numbers = [];
     },
+    update: function(result)
+    {
+        this.numbers[0].value = result;
+    },
+    prevOperation: '',
     calculate: function()
     {
+        var operator = this.numbers[0].operator,
+            firstNumber = this.numbers[0].value,
+            secondNumber = this.numbers[1].value || null,
+            that = this,
+            result;
 
+        function operation(operator, result, operation) {
+            historyData.addHistory(firstNumber.toString() + ' ' + operator + ' ' + secondNumber.toString() + ' = ' + result.toString(), displayTime('history'));
+            that.update(result.toString());
+            that.displayCalculation(result);
+            that.prevOperation = operation;
+            historyData.displayHistory();
+        }
+
+        switch(operator)
+        {
+            case 'power':
+                result = Math.pow(parseFloat(firstNumber), parseFloat(secondNumber));
+                operation('^', result, 'power');
+                break;
+            case 'division':
+                result = parseFloat(firstNumber / secondNumber);
+                operation('÷', result, 'division');
+                break;
+            case 'multiplication':
+                result = parseFloat(firstNumber * secondNumber);
+                operation('×', result, 'multiplication');
+                break;
+            case 'addition':
+                result = parseFloat(firstNumber + secondNumber);
+                operation('+', result, 'addition');
+                break;
+            case 'subtraction':
+                result = parseFloat(firstNumber - secondNumber);
+                operation('-', result, 'subtraction');
+                break;
+        }
+
+    },
+    displayCalculation: function(result)
+    {
+        var topScreen = document.getElementById('top-calculation'),
+            bottomScreen = document.getElementById('bottom-calculation');
+
+        topScreen.innerHTML = '';
+        bottomScreen.innerHTML = result;
     }
 };
 
 var historyData =
 {
     history: [],
-    addHistory: function(number, calculation)
+    addHistory: function(calculation, time)
     {
         this.history.push({
-            number: number,
-            calculation: calculation
+            calculation: calculation,
+            time: time
         });
+    },
+    removeOne: function(position) {
+        this.history.splice(position, 1);
+    },
+    removeAll: function() {
+        this.history = [];
     },
     displayHistory: function()
     {
@@ -72,9 +130,13 @@ var historyData =
         {
             var historyLi = document.createElement('li');
             historyLi.id = position;
-            historyLi.textContent = history.calculation;
+            historyLi.textContent = history.time + ' | ' + history.calculation;
             historyUl.appendChild(historyLi);
         }, this);
+    },
+    deleteButton: function() 
+    {
+        var button = document.createElement('button');
     }
 };
 
@@ -95,7 +157,6 @@ function events()
                 button.classList.add('keypress');
                 value = button.innerHTML;
                 // Call buttonHandlers function to 'deal' with button value
-                console.log(value);
                 buttonValueHandler(value);
             }
             buttons.forEach(function(button)
@@ -125,8 +186,8 @@ function events()
 
 // Global variables
 var number = '',
-    prevOperation = '',
-    defaultValue = '0';
+    defaultValue = '0',
+    prevClick;
 
 // To set zero at starting point
 document.getElementById('bottom-calculation').innerHTML = defaultValue;
@@ -143,6 +204,8 @@ function buttonValueHandler(value)
             topScreen.innerHTML = '';
             bottomScreen.innerHTML = '';
             number = '';
+            calculationData.clearNumbers();
+            prevClick = 'clearAll';
             break;
         case '<span class="ion-backspace"></span>':
             number = number.slice(0, -1);
@@ -151,19 +214,7 @@ function buttonValueHandler(value)
         case '+/-':
             if(number.match(/\d+/) !== null)
             {
-                var history = historyData.history;
-                if(prevOperation === 'negate' && parseFloat(number) === -parseFloat(history[history.length - 1].number))
-                {
-                    historyData.addHistory(number.toString() ,'negate(' + history[history.length - 1].calculation + ')');
-                }
-                else
-                {
-                    historyData.addHistory(number.toString() ,'negate(' + number + ')');
-                }
-                number = (-parseFloat(number)).toString();
-                bottomScreen.innerHTML = number;
-                historyData.displayHistory();
-                prevOperation = 'negate';
+                prevClick = 'negate';
             }
             break;
         case 'x<sup>y</sup>':
@@ -171,27 +222,35 @@ function buttonValueHandler(value)
             if(number.match(/\d+/) !== null)
             {
                 calculationData.addNumber(number, 'power');
-                topScreen.innerHTML = number + value;
+                topScreen.innerHTML = number + ' ' + value;
                 number = '';
-                prevOperation = 'power';
+                prevClick = 'power';
             }
-
             break;
         case '÷':
             value = '/';
+            if(number.match(/\d+/) !== null && calculationData.numbers.length === 0)
+            {
+                calculationData.addNumber(number, 'division');
+                topScreen.innerHTML = number + ' ' + value;
+                number = '';
+                prevClick = 'division';
+            }
             break;
 
         case '=':
-
+            calculationData.addNumber(number);
+            calculationData.calculate();
+            number = '';
+            prevClick = 'result';
             break;
         default:
+            if(prevClick === 'result')
+            {
+                calculationData.clearNumbers();
+            }
             number  += value;
             bottomScreen.innerHTML = number;
-    }
-    // To set zero value on bottom screen if any other values are not present
-    if(number === '')
-    {
-        bottomScreen.innerHTML = defaultValue;
     }
 }
 
